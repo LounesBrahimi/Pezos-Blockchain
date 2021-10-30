8aa6677dfb78ec81d58f6facba7e9f76eb0d86a0
LounesBrahimi <Lounesnewgs5@gmail.com>
2021-10-30 14:08:40 +0200

    restructurisation et netoyage

---
+++ pezos/src/pezos/Main.java
@@ -0,0 +1,14 @@
+package pezos;
+
+public class Main {
+	//static String hostname = "78.194.168.67";
+//	static int    port     = 1337;
+//	static String pk       = "b8b606dba2410e1f3c3486e0d548a3053ba3f907860fada6fab2835fb27b3f21"; // public
+//	static String sk       = "1f06949f1278fcbc0590991180d5b567d240c0b0576d1d34cad66db49d4eea4a"; // secret
+
+	public static void main(String[] args) {
+		//Connection connection = new Connection(hostname,port,sk,pk);
+		//new IterationLoop(connection); 
+	}
+
+}
