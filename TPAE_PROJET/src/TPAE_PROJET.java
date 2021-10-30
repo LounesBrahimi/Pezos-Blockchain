be8e4e7eb6034a355603615e2c376514f18abf6c
gitthubbann <photo2000photo@gmail.com>
2021-10-30 12:31:33 +0200

    enlever TME-2

--- TPAE_PROJET/src/TPAE_PROJET.java
+++ TPAE_PROJET/src/TPAE_PROJET.java
@@ -19,18 +19,6 @@ public class TPAE_PROJET {
 		//Block testBlock = new Block(44,"1c80203a30e5de4d980cc555131d1b4a4750edc82c0c443179d88de1ae4f6cdf","2021-10-10 15:21:09","0000000000000000000000000000000000000000000000000000000000000000","22a00d1c8c0fbaefedd71ddb83d455033efd259a8f0adf189b9f850a0d1945f2","cc3faffc696c86db13d50752fdb7edd0ee1ce19ab350f60899939fc139d58996419c13b812b7f005fafaf23924d2f1df555036bc61e7b67cb679375e5756b306");
 		//System.out.println(testBlock+"\n");
 		
-		ArrayList<String> dataArray = new ArrayList<String>(); // size = 2^n
-		dataArray.add("a");
-		dataArray.add("b");
-		dataArray.add("c");
-		dataArray.add("d");
-		dataArray.add("e");
-		dataArray.add("f");
-		dataArray.add("g");
-		dataArray.add("h");
-		MerkleTree tree = new MerkleTree(dataArray);
-		System.out.println("TREE      : "+tree);
-		
 		Connection connection = new Connection(hostname,port,sk,pk);
 		new IterationLoop(connection); 
 	}
