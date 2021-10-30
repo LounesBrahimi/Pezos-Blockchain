35b96801db0187b96aedac83d7ebb71e59469216
LounesBrahimi <Lounesnewgs5@gmail.com>
2021-10-30 14:12:05 +0200

    restructurisation de la class Connection

--- pezos/src/connection/Connection.java
+++ pezos/src/connection/Connection.java
@@ -13,28 +13,26 @@ import java.net.UnknownHostException;
  * */
 
 public class Connection {
-	public DataOutputStream out = null;
-	public DataInputStream  in  = null;
 	
 	public Connection(String hostname, int port, String skString, String pkString) throws UnknownHostException, IOException {
 
 			Socket socket = new Socket(hostname, port); 
-			this.in         = new DataInputStream (new BufferedInputStream (socket.getInputStream ()));
-			this.out        = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+			DataInputStream  in	= new DataInputStream (new BufferedInputStream (socket.getInputStream ()));
+			DataOutputStream out = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
 			
 			////// 1st message = seed
-			//byte[] seed = Utils.getFromSocket(24,this.in,"seed"); 
+			byte[] seed = Utils.getFromSocket(24,this.in,"seed"); 
 			//seed = Utils.toBytesArray("12230025a6118122e9eac8785c74193819441fe57fec4845"); System.out.println("seed = "+Utils.toStringOfHex(seed));
 
 			/////// 2nd message = pk
-			//Utils.sendToSocket (pkString,this.out,"pk");
+			Utils.sendToSocket (pkString,this.out,"pk");
 
 			/////// 3d message = signature of hashSeed
-		//	byte[] hashSeed = Utils.hash(seed, 32);
-	//		byte[] signature = Utils.signature(hashSeed, skString);
+			byte[] hashSeed = Utils.hash(seed, 32);
+			byte[] signature = Utils.signature(hashSeed, skString);
 			//System.out.println("hashSeed  = " + Utils.toStringOfHex(hashSeed ));
 			//System.out.println("signature = " + Utils.toStringOfHex(signature));
-	//		Utils.sendToSocket(signature,this.out,"signature");
+			Utils.sendToSocket(signature,this.out,"signature");
 		} 
 	
 	// fermer a la fin les ports et flux.
